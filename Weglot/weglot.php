9885570efc39478ab0757b3e47f60fd4ff4abe7a
weglot <remy@weglot.com>
2016-09-22 11:20:14 +0200

    Adding Weglot PHP lib
    upload files

---
+++ Weglot/weglot.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'src/WG.php';
+require_once 'src/WGClient.php';
+require_once 'src/simple_html_dom.php';
+require_once 'src/WGUtils.php';
+require_once 'src/WeglotException.php';
