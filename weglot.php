438db56dfe8c55ad5e3e88be0096fd13d9ca737f
weglot <remy@weglot.com>
2016-09-25 16:36:00 +0200

    Add files via upload

---
+++ weglot.php
@@ -0,0 +1,6 @@
+<?php
+
+require(dirname(__FILE__) . '/lib/WG.php');
+require(dirname(__FILE__) . '/lib/WGClient.php');
+require(dirname(__FILE__) . '/lib/WGUtils.php');
+require(dirname(__FILE__) . '/lib/WeglotException.php');
