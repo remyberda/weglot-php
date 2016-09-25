af7e260ed11ac5faeac85650fa9d2341926c606b
weglot <remy@weglot.com>
2016-09-25 16:35:20 +0200

    Delete weglot.php

--- weglot.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require(dirname(__FILE__) . '/lib/WG.php');
-require(dirname(__FILE__) . '/lib/WGClient.php');
-require(dirname(__FILE__) . '/lib/simple_html_dom.php');
-require(dirname(__FILE__) . '/lib/WGUtils.php');
-require(dirname(__FILE__) . '/lib/WeglotException.php');
